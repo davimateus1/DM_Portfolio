f010919252f53ee7b4f093e4807e954ade0f5065
Davi M. G. de Almeida <davi.2020109493@unicap.br>
2022-02-09 15:32:00 -0300

    footer adjustments

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,49 +3,71 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <div className={styles.title}>
+        <h2>Davi</h2>
+      </div>
+
       <ul className={styles.social_list}>
         <li>
           <a href="https://www.instagram.com/davimateus1/" target="_blank">
-            {/* <FaInstagram /> */}
             Inicio
           </a>
         </li>
         <li>
           <a href="https://github.com/davimateus1" target="_blank">
-            {/* <FaGithub /> */}
             Contate-me
           </a>
         </li>
         <li>
           <a href="https://www.linkedin.com/in/davimateusg/" target="_blank">
-            {/* <FaLinkedin /> */}
             Social
           </a>
         </li>
       </ul>
-      <ul className={styles.social_list}>
+
+      <ul className={styles.social_list_two}>
         <li>
           <a href="https://www.instagram.com/davimateus1/" target="_blank">
-            {/* <FaInstagram /> */}
             Inicio
           </a>
         </li>
         <li>
           <a href="https://github.com/davimateus1" target="_blank">
-            {/* <FaGithub /> */}
             Contate-me
           </a>
         </li>
         <li>
           <a href="https://www.linkedin.com/in/davimateusg/" target="_blank">
-            {/* <FaLinkedin /> */}
             Social
           </a>
         </li>
       </ul>
-      <br/>
+
+      <ul className={styles.social_list_three}>
+        <li>
+          <a href="https://www.instagram.com/davimateus1/" target="_blank">
+            Inicio
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/davimateus1" target="_blank">
+            Contate-me
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/davimateusg/" target="_blank">
+            Social
+          </a>
+        </li>
+      </ul>
+
+      <br />
       <hr />
-      &copy;
+
+      <div className={styles.authorized}>
+        <p>Davi &copy; 2021</p>
+      </div>
+
     </footer>
   );
 };
