63a59ad36b32a28c6769fffd0cc2e63702f63b74
Davi Mateus <davi.2020109493@unicap.br>
2022-02-09 21:00:52 -0300

    added react router and created new page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./pages/HomePage/HomePage";
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route element={<HomePage />} exact path="/" />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 };
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import styles from "./HomePage.module.css"
+
+const HomePage = () => {
+    return(
+        <div className={styles.container}>
+            <h1>HomePage</h1>
+        </div>
+    )
+}
+
+export default HomePage;
