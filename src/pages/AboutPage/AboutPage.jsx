479ee0304c3c344f4d1480c83edcae5ae346fffa
Davi Mateus <davi.2020109493@unicap.br>
2022-02-12 22:23:41 -0300

    started about page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,7 +1,38 @@
-const AboutPage = () => {
-    return (
-        <h1>Página sobre mim</h1>
-    )
-}
+import { Link } from "react-router-dom";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
-export default AboutPage
+import styles from "./AboutPage.module.css";
+
+import davi from "../../assets/img/davi.png";
+
+const AboutPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.back}>
+        <Link to="/">
+          <AiOutlineArrowLeft /> Voltar
+        </Link>
+      </div>
+      <div className={styles.card_home}>
+        <div className={styles.image_container}>
+          <img src={davi} alt="avatar" />
+        </div>
+        <div className={styles.texts}>
+          <h1>Sobre mim</h1>
+          <p>
+            Meu nome é Davi Mateus, tenho 19 anos, curso Ciência da Computação
+            na Universidade Católica de Pernambuco (UNICAP). Desde pequeno,
+            sempre gostei muito de coisas que eram voltadas a tecnologia e
+            jogos, mas infelizmente só vim conhecer o mundo da programação no
+            final do 3° ano do ensino médio. Quando comecei o curso, estudei as
+            áreas e me apaixonei pela área de front-end, hoje sou desenvolvedor
+            e continuo sempre estudando para descobrir novas coisas e aprimorar
+            as minhas habilidades!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
