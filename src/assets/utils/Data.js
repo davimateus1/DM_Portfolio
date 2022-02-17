ca44033cb9e6f6bf0e6aa1baa529e8bdb06496c2
Davi Mateus <davi.2020109493@unicap.br>
2022-02-16 23:52:31 -0300

    adjustment java card

--- src/assets/utils/Data.js
+++ src/assets/utils/Data.js
@@ -20,21 +20,21 @@ export const CardsHome = [
     id: 1,
     title: "HTML",
     subtitle:
-      "Essa tecnologia foi uma das primeiras que comecei a estudar, a linguagem de marcação HTML5, participei de vários cursos sobre. E com ela construi vários sites juntamente com CSS e JavaScript.",
+      "Essa tecnologia foi uma das primeiras que comecei a estudar, a linguagem de marcação HTML5. Participei de vários cursos sobre e com ela construi vários sites juntamente com CSS e JavaScript.",
     svg: DiHtml5,
   },
   {
     id: 2,
     title: "CSS",
     subtitle:
-      "Aprendendo o HTML seria mais do que minha obrigação aprender o CSS, levando isso em consideração, estudei em conjunto, para entender melhor a comunicação entre ele e o HTML.",
+      "Aprendendo o HTML seria mais do que minha obrigação aprender o CSS, levando isso em consideração estudei em conjunto para entender melhor a comunicação entre ele e o HTML.",
     svg: DiCss3,
   },
   {
     id: 3,
     title: "ReactJS",
     subtitle:
-      "Logo após entender entender HTML, CSS e JavaScript, decidi tomar a iniciativa de aprender uma biblioteca, e o que melhor combina essas três tecnologias é o ReactJS, inclusive (spoiler) este portfólio está sendo desenvolvido com o mesmo.",
+      "Logo após entender entender HTML, CSS e JavaScript decidi tomar a iniciativa de aprender uma biblioteca, e o que melhor combina essas três tecnologias é o ReactJS, inclusive (spoiler) este portfólio está sendo desenvolvido com o mesmo.",
     svg: DiReact,
   },
 ];
@@ -44,14 +44,14 @@ export const CardsAbout = [
     id: 1,
     title: "HTML",
     subtitle:
-      "Essa tecnologia foi uma das primeiras que comecei a estudar, a linguagem de marcação HTML5, participei de vários cursos sobre. E com ela construi vários sites juntamente com CSS e JavaScript.",
+      "Essa tecnologia foi uma das primeiras que comecei a estudar, a linguagem de marcação HTML5. Participei de vários cursos sobre e com ela construi vários sites juntamente com CSS e JavaScript.",
     svg: DiHtml5,
   },
   {
     id: 2,
     title: "CSS",
     subtitle:
-      "Aprendendo o HTML seria mais do que minha obrigação aprender o CSS, levando isso em consideração, estudei em conjunto, para entender melhor a comunicação entre ele e o HTML.",
+      "Aprendendo o HTML seria mais do que minha obrigação aprender o CSS, levando isso em consideração estudei em conjunto para entender melhor a comunicação entre ele e o HTML.",
     svg: DiCss3,
   },
   {
@@ -65,21 +65,21 @@ export const CardsAbout = [
     id: 4,
     title: "Bootstrap",
     subtitle:
-      "Conheci o Bootstrap através do curso Web Development - Frontend, após conhece-lo, percebi o quão importante era ter ele em minhas Skills, pelo fato da responsividade e estilização que era muito prática e minimalista.",
+      "Conheci o Bootstrap através do curso Web Development - Frontend, após conhece-lo percebi o quão importante era ter ele em minhas Skills, pelo fato da responsividade e estilização que era muito prática e minimalista.",
     svg: SiBootstrap,
   },
   {
     id: 5,
     title: "ReactJS",
     subtitle:
-      "Logo após entender entender HTML, CSS e JavaScript, decidi tomar a iniciativa de aprender uma biblioteca, e o que melhor combina essas três tecnologias é o ReactJS, inclusive (spoiler) este portfólio está sendo desenvolvido com o mesmo.",
+      "Logo após entender entender HTML, CSS e JavaScript decidi tomar a iniciativa de aprender uma biblioteca, e o que melhor combina essas três tecnologias é o ReactJS, inclusive (spoiler) este portfólio está sendo desenvolvido com o mesmo.",
     svg: DiReact,
   },
   {
     id: 6,
     title: "Java",
     subtitle:
-      "Logo após entender entender HTML, CSS e JavaScript, decidi tomar a iniciativa de aprender uma biblioteca, e o que melhor combina essas três tecnologias é o ReactJS, inclusive (spoiler) este portfólio está sendo desenvolvido com o mesmo.",
+      "O Java foi a linguagem que eu tive meu primeiro contato e desenvolvi a lógica de programação essencial, trabalhei com ela em todos os periodos da faculdade (até o momento), assim adquiri um certo conhecimento e dominio na linguagem.",
     svg: DiJava,
   },
 ];
@@ -89,7 +89,7 @@ export const CardsStudy = [
     id: 1,
     title: "NextJS",
     subtitle:
-      "Estou estudando NextJS pelo fato da sua simplicidade em exercer algumas funções, uma das que mais me chama atenção é a facilidade em criar uma rota, que você precisa apenas criar um arquivo dentro de uma pasta pages. Fora isso, quero aprender boa parte das suas funcionalidades.",
+      "Estou estudando NextJS pelo fato da sua simplicidade em exercer algumas funções, uma das que mais me chama atenção é a facilidade em criar uma rota, que você precisa apenas criar um arquivo dentro de uma pasta pages. Fora isso, quero aprender grande parte das suas funcionalidades.",
     svg: SiNextdotjs,
   },
   {
